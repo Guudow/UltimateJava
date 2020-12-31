a6652fcc72c2b08b70febd9a02decd27e0014d4c
guudow <mguudow@aol.com>
2020-12-31 22:36:05 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.text.NumberFormat;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        String result = percent.format(0.12);
+        System.out.println(result);
+//        int[] numbers = {1,2,4,5,6,7,7};
+//        System.out.println(numbers.length);
+//        System.out.println(Arrays.toString(numbers));
+    }
+}
